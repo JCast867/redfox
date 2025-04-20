f15a6117e137d01e260fb582115c7d0031cf52f7
Jaime <jaiimecast04@gmail.com>
2025-04-19 23:17:00 -0500

    carousel images added

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,73 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
-import React from "react";
+import Carousel from 'react-bootstrap/Carousel';
+import React, { useState } from "react";
+import image1 from "../images/img1.jpg"
+import image2 from "../images/img2.jpg"
+import image3 from "../images/img3.jpg"
+import image4 from "../images/img4.jpg"
+import image5 from "../images/img5.jpg"
+import image6 from "../images/img6.jpg"
+import image7 from "../images/img7.jpg"
+import image8 from "../images/img8.jpg"
+import image9 from "../images/img9.jpg"
+import image10 from "../images/img10.jpg"
+import image11 from "../images/img11.jpg"
+import image12 from "../images/img12.jpg"
+import image13 from "../images/img13.jpg"
+import image14 from "../images/img14.jpg"
+import image15 from "../images/img15.jpg"
+import image16 from "../images/img16.jpg"
+import image17 from "../images/img17.jpg"
+import image18 from "../images/img18.jpg"
+import image19 from "../images/img19.jpg"
+import '../styles/CarouselStyles.css';
+
 
 function Main() {
+    const redfox_images = [
+        image1,
+        image2,
+        image3,
+        image4,
+        image5,
+        image6,
+        image7,
+        image8,
+        image9,
+        image10,
+        image11,
+        image12,
+        image13,
+        image14,
+        image15,
+        image16,
+        image17,
+        image18,
+        image19
+    ];
+
+    const [images] = useState(redfox_images);
+
+
     return (
-        <Container>
+        <Container className="mb-4">
             <h2 className="text-center mb-3">Quality Roof Construction</h2>
             <h6 className="text-center mb-5">Tailored Services for Your Property</h6>
+
+            <Carousel className="image-carousel mb-5">
+                {images.map((image, index) => (
+                    <Carousel.Item key={index}>
+                        <img
+                            className="mx-auto d-block w-75"
+                            height="600px"
+                            width="100px"
+                            src={image}
+                            alt={`Slide ${index + 1}`}
+                        />
+                    </Carousel.Item>
+                ))}
+            </Carousel>
 
         </Container>
     )
